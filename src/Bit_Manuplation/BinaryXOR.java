6e6c68693cb68255c99556310e172dfc2a6cac9b
Harsh Kumar <harshkum2025@gmail.com>
2026-07-12 23:45:23 +0530

    "Finish Bitwise XOR"

---
+++ src/Bit_Manuplation/BinaryXOR.java
@@ -0,0 +1,13 @@
+package Bit_Manuplation;
+
+public class BinaryXOR {
+    // Binary XOR
+    // 0 ^ 0 = 0
+    // 0 ^ 1 = 1
+    // 1 ^ 0 = 1
+    // 1 ^ 1 = 0
+
+    public static void main(String[] args) {
+        System.out.println((5 ^ 6));
+    }
+}
