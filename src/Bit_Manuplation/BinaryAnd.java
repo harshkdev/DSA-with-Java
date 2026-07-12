06b146881ce1a35239cbdaca0b30cae8b7c300b1
Harsh Kumar <harshkum2025@gmail.com>
2026-07-12 23:33:41 +0530

    "String Compression"

---
+++ src/Bit_Manuplation/BinaryAnd.java
@@ -0,0 +1,11 @@
+package Bit_Manuplation;
+
+public class BinaryAnd {
+    // 0 & 0 = 0
+    // 0 & 1 = 0
+    // 1 & 0 = 0
+    // 1 & 1 = 1
+    public static void main(String[] args) {
+        System.out.println((5 & 6));
+    }
+}
--- src/Strings/StringCompression.java
+++ src/Strings/StringCompression.java
@@ -1,7 +1,26 @@
-class Solution {
-    public int[] arrayRankTransform(int[] arr) {
+package Strings;
+
+public class StringCompression {
 
 
+    public static String compress(String str){
+        String newStr = "";
 
+        for (int i=0; i<str.length()-1; i++){
+            Integer count = 1;
+            while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1){
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+    public static void main(String[] args) {
+        String str = "aaabbcccdd";
+        System.out.println(compress(str));
     }
 }
