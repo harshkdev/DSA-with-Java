721f845cf1b7d969b94c005eca429ab59624e355
Harsh Kumar <harshkum2025@gmail.com>
2026-07-12 23:38:26 +0530

    "String Compression"

---
+++ src/Bit_Manuplation/BinaryOR.java
@@ -0,0 +1,14 @@
+package Bit_Manuplation;
+
+public class BinaryOR {
+
+    //Binary OR |
+    // 0 | 0 = 1
+    // 0 | 1 = 1
+    // 1 | 0 = 1
+    // 1 | 1 = 1
+
+    public static void main(String[] args) {
+        System.out.println((5 | 6));
+    }
+}
