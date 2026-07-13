aedb49efd456a2c38e57470e020db3c44860f663
Harsh Kumar <harshkum2025@gmail.com>
2026-07-13 19:53:01 +0530

    "clearIthBit"

---
+++ src/OOPS/OOPS.java
@@ -0,0 +1,40 @@
+package OOPS;
+
+public class OOPS {
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // Create a pen object p1
+        p1.setColor("blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+
+        //p1.setColor("Yellow"// )
+        p1.color = "Yellow";
+        System.out.println(p1.color);
+
+    }
+}
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage; //cgpa
+
+    void calPercentage(int phy , int chem , int math){
+        percentage = (phy + chem + math) / 3;
+    }
+}
+
