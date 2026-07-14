1a1b23ad34c6707125b6994d36b3d8ff0620918b
Harsh Kumar <harshkum2025@gmail.com>
2026-07-14 17:35:55 +0530

    "Constructors"

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -29,12 +29,23 @@ class Student{
     int roll;
     int marks[];
 
-    //Copy Constructors
+//    //shallow Copy Constructors
+//    Student(Student s1){
+//        marks = new int[3];
+//        this.name = s1.name;
+//        this.roll = s1.roll;
+//        this.marks = s1.marks;
+//    }
+
+    //deep copy constructor
     Student(Student s1){
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
-        this.marks = s1.marks;
+        for (int i = 0; i < marks.length; i++){
+            this.marks[i] = s1.marks[i];
+        }
+
     }
 
     Student(){
