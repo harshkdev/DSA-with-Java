86ace471ac453d07a32b873f270742c7cdb90ae1
Harsh Kumar <harshkum2025@gmail.com>
2026-07-14 17:07:12 +0530

    "Constructors"

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -1,29 +1,53 @@
 package OOPS;
 
-import java.sql.SQLOutput;
 
 public class Constructor {
 
     public static void main (String[] args){
-        Student s1 = new Student();
-        Student s2 = new Student("Harsh");
-        Student s3 = new Student(123);
-//        Student s4 = new Student("aman" , 567); Error
-        System.out.println(s1.name);
+       Student s1 = new Student();
+       s1.name = "Harsh Kumar";
+       s1.roll = 456;
+       s1.password = "abcd";
+       s1.marks[0] = 100;
+       s1.marks[1] = 90;
+       s1.marks[2] = 80;
+
+        Student s2 = new Student(s1);//copy the values into s1
+        s2.password = "xyz";
+        s1.marks[2] = 100;
+
+
+       for (int i = 0; i<3; i++){
+           System.out.println(s2.marks[i]);
+       }
     }
 }
 
 class Student{
+    String password;
     String name;
     int roll;
+    int marks[];
+
+    //Copy Constructors
+    Student(Student s1){
+        marks = new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        this.marks = s1.marks;
+    }
 
     Student(){
+        marks = new int[3];
         System.out.println("Constructor is called...");
     }
     Student(String name){
+        marks = new int[3];
         this.name = name;
+
     }
     Student(int roll){
+        marks = new int[3];
         this.roll = roll;
     }
 }
