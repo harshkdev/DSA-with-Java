9711cb067933a3ed8f2e7e0f6713c5039313724f
Harsh Kumar <harshkum2025@gmail.com>
2026-07-14 19:57:13 +0530

    "MethodsOverriding]"

---
+++ src/OOPS/MethofdOverriding.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class MethofdOverriding {
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animals{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+
+class Deer extends Animals{
+    @Override
+    void eat() {
+        System.out.println("eats grass");
+    }
+}
