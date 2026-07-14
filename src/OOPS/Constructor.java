c167915a9cb532fbd60c6c07571d2a912790056b
Harsh Kumar <harshkum2025@gmail.com>
2026-07-14 16:34:59 +0530

    "Constructors"

--- src/OOPS/Constructor.java
+++ src/OOPS/Constructor.java
@@ -1,9 +1,14 @@
 package OOPS;
 
+import java.sql.SQLOutput;
+
 public class Constructor {
 
     public static void main (String[] args){
-        Student s1 = new Student("Harsh");
+        Student s1 = new Student();
+        Student s2 = new Student("Harsh");
+        Student s3 = new Student(123);
+//        Student s4 = new Student("aman" , 567); Error
         System.out.println(s1.name);
     }
 }
@@ -12,7 +17,13 @@ class Student{
     String name;
     int roll;
 
+    Student(){
+        System.out.println("Constructor is called...");
+    }
     Student(String name){
         this.name = name;
+    }
+    Student(int roll){
+        this.roll = roll;
     }
 }
