21aa55f1ec23518c9c49b68d56ba3fcead5f5937
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 21:43:14 +0530

    "xToThePower (optimized Code)"

--- src/Recursion/LastOccurrence.java
+++ src/Recursion/LastOccurrence.java
@@ -24,8 +24,24 @@ public class LastOccurrence {
         return x * power(x , n-1);
     }
 
+    public static int optimizedPower(int a, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPower = optimizedPower(a, n / 2);
+        int halfPowerSq = halfPower * halfPower;
+
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
+
     public static void main(String[] args) {
         int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
-        System.out.println(power(2 , 4));
+        System.out.println(optimizedPower(2 , 10));
     }
 }
