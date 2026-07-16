18d8f26de0d061392fbb074ca7898406511302d2
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 14:19:13 +0530

    "Print Decreasing number"

--- src/Recursion/PrintNumber.java
+++ src/Recursion/PrintNumber.java
@@ -3,11 +3,13 @@ package Recursion;
 public class PrintNumber {
 
     public static void printDec(int n){
+        //Base value
         if (n == 1){
             System.out.println(n);
             return;
         }
         System.out.print(n+" ");
+        //Recursive function
         printDec(n-1);
 
     }
