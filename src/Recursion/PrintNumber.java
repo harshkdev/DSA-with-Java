467a9b3e9eef2a473b490a61f528199c1cf558ef
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 14:50:15 +0530

    "Print Increasing number"

--- src/Recursion/PrintNumber.java
+++ src/Recursion/PrintNumber.java
@@ -13,8 +13,17 @@ public class PrintNumber {
         printDec(n-1);
 
     }
+
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
         int n = 10;
-        printDec(n);
+        printInc(n);
     }
 }
