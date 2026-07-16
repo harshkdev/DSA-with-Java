a905805325a6f72c4a339346ecb13644062bbb65
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 16:28:40 +0530

    "Factorial"

---
+++ src/Recursion/PrintFact.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class PrintFact {
+
+    public static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        int fact = n * fact(n - 1);
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(fact(5));
+    }
+}
