2b18c0a2415fb92c0e8e4621806a8354b72548a7
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 19:52:10 +0530

    "xToThePower"

--- src/Recursion/LastOccurrence.java
+++ src/Recursion/LastOccurrence.java
@@ -13,8 +13,19 @@ public class LastOccurrence {
         return isFound;
     }
 
+    public static int power(int x , int n){
+        if (n == 0){
+            return 1;
+        }
+//        int xnm1 = power(x , n-1);
+//        int xn = x * xnm1;
+//        return xn;
+
+        return x * power(x , n-1);
+    }
+
     public static void main(String[] args) {
         int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
-        System.out.println(lastOccurrence(arr, 5, 0));
+        System.out.println(power(2 , 4));
     }
 }
