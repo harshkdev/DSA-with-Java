c23a2bb59a57855daed32292040ca126ac2dbdc8
Harsh Kumar <harshkum2025@gmail.com>
2026-07-16 17:51:44 +0530

    "First Occurrence"

---
+++ src/Recursion/FirstOccurrence.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class FirstOccurrence {
+
+    public static int Occur(int arr[], int key , int i){
+        if (i == arr.length){
+            return -1;
+        }
+        if (arr[i] == key){
+            return i;
+        }
+        return Occur( arr , key,i + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 5, 67, 8, 5};
+        int key= 67;
+        System.out.println(Occur(arr , key , 0));
+    }
+}
