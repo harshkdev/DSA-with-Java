8749763c09edea60544559843e1377ec55bab20d
Harsh Kumar <harshkum2025@gmail.com>
2026-07-28 18:48:32 +0530

    "Friends Pairing Problem"

---
+++ src/Recursion/FriendsPariningProblem.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class FriendsPariningProblem {
+    public static int friendsPairing(int n){
+        if (n == 1 || n == 2){
+            return n;
+        }
+        //Choices
+
+        //Single
+        int fnm1 = friendsPairing(n - 1);
+
+        //Pair
+        int fnm2 = friendsPairing(n - 2);
+        int pairWays = (n - 1) * fnm2;
+
+        //totWays
+        int totWays = fnm1 + pairWays;
+        return totWays;
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(friendsPairing(3));
+    }
+}
